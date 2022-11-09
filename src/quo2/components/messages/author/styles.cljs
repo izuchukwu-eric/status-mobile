59cefb57ecd451546742d2327caeb42ec8dc6002
Brian Sztamfater <briansztamfater@MacBook-Air-de-Brian.local>
2022-11-09 11:57:12 -0300

    move styles from author component to style file

--- src/quo2/components/messages/author/styles.cljs
+++ src/quo2/components/messages/author/styles.cljs
@@ -1,4 +1,5 @@
-(ns quo2.components.messages.author.styles)
+(ns quo2.components.messages.author.styles
+  (:require [quo2.foundations.colors :as colors]))
 
 (def container
   {:flex   1
@@ -6,3 +7,33 @@
    :height 18
    :flex-direction :row
    :align-items    :center})
+
+(def ens-text
+  {:color (colors/theme-colors colors/neutral-100 colors/white)})
+
+(def nickname-text
+  {:color (colors/theme-colors colors/neutral-100 colors/white)})
+
+(def middle-dot-nickname
+  {:color             colors/neutral-50
+   :margin-horizontal 4})
+
+(def public-key-text
+  {:color       colors/neutral-50
+   :margin-left 8})
+
+(def middle-dot-public-key
+  {:color       colors/neutral-50
+   :margin-left 4})
+
+(defn profile-name-text [has-nickname?]
+  {:color (if has-nickname?
+            (colors/theme-colors colors/neutral-60 colors/neutral-40)
+            (colors/theme-colors colors/neutral-100 colors/white))})
+
+(def icon-container
+  {:margin-left 4})
+
+(defn time-text [is-ens?]
+  {:color       colors/neutral-50
+   :margin-left (if is-ens? 8 4)})
--- src/quo2/components/messages/author/view.cljs
+++ src/quo2/components/messages/author/view.cljs
@@ -16,50 +16,46 @@
           [text/text {:weight :semi-bold
                       :size   :paragraph-2
                       :color  (colors/theme-colors colors/neutral-100 colors/white)
-                      :style  {:color (colors/theme-colors colors/neutral-100 colors/white)}}
+                      :style  styles/ens-text}
            ens-name]
           [:<>
            (when has-nickname?
              [:<>
               [text/text {:weight :semi-bold
-                          :size      :paragraph-2
-                          :style     {:color (colors/theme-colors colors/neutral-100 colors/white)}}
+                          :size   :paragraph-2
+                          :style  styles/nickname-text}
                nickname]
-              [text/text {:size      :paragraph-2
-                          :style     {:color       colors/neutral-50
-                                      :margin-horizontal 4}}
+              [text/text {:size  :paragraph-2
+                          :style styles/middle-dot-nickname}
                "·"]])
            [text/text {:weight (if has-nickname? :medium :semi-bold)
                        :size   :paragraph-2
-                       :style {:color (if has-nickname? (colors/theme-colors colors/neutral-60 colors/neutral-40) (colors/theme-colors colors/neutral-100 colors/white))}}
+                       :style  (styles/profile-name-text has-nickname?)}
             profile-name]])
         (when is-contact?
-          [icons/icon :main-icons2/contact {:size 12
-                                            :no-color true
-                                            :container-style {:margin-left 4}}])
+          [icons/icon :main-icons2/contact {:size            12
+                                            :no-color        true
+                                            :container-style styles/icon-container}])
         (cond
           is-verified?
-          [icons/icon :main-icons2/verified {:size 12
-                                             :no-color true
-                                             :container-style {:margin-left 4}}]
+          [icons/icon :main-icons2/verified {:size            12
+                                             :no-color        true
+                                             :container-style styles/icon-container}]
           is-untrustworthy?
-          [icons/icon :main-icons2/untrustworthy {:size 12
-                                                  :no-color true
-                                                  :container-style {:margin-left 4}}])
+          [icons/icon :main-icons2/untrustworthy {:size            12
+                                                  :no-color        true
+                                                  :container-style styles/icon-container}])
         (when-not is-ens?
           [text/text {:monospace true
                       :size      :paragraph-2
-                      :style     {:color       colors/neutral-50
-                                  :margin-left 8}}
+                      :style     styles/public-key-text}
            public-key])
         (when-not is-ens?
           [text/text {:monospace true
                       :size      :paragraph-2
-                      :style     {:color       colors/neutral-50
-                                  :margin-left 4}}
+                      :style     styles/middle-dot-public-key}
            "·"])
         [text/text {:monospace true
                     :size      :paragraph-2
-                    :style     {:color       colors/neutral-50
-                                :margin-left (if is-ens? 8 4)}}
+                    :style     (styles/time-text is-ens?)}
          time-str]]))])
